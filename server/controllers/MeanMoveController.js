f11171d569771ca5d6938e1d516d2ac50a216863
kat markava <katryc@outlook.com>
2016-03-27 17:28:21 -0400

    created meanmove-controller

---
+++ server/controllers/MeanMoveController.js
@@ -0,0 +1,17 @@
+var restful = require('node-restful');
+
+module, exports = function(app, route){
+
+
+//pass the model to the restful module
+  var rest = restful.model('meanmove', app.models.movie).methods(
+    ['get', 'put', 'post', 'delete']);
+
+//register the restful API for the injected app into the controller
+  rest.register(app,route);
+//for every injection on the requestt we use custom features with the help of middleware
+   return function(req, res, next){
+     next();
+   };      
+
+};
