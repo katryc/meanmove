fe04d6728faf9a1b165c85002fa791554eba7cf8
kat markava <katryc@outlook.com>
2016-03-28 11:08:47 -0400

    fixed controllers, got restful to work

--- server/controllers/MeanMoveController.js
+++ server/controllers/MeanMoveController.js
@@ -1,16 +1,19 @@
 var restful = require('node-restful');
-
-module, exports = function(app, route){
+module.exports = function(app, route) {
 
 
 //pass the model to the restful module
-  var rest = restful.model('meanmove', app.models.meanmove).methods(['get', 'put', 'post', 'delete']);
+// Setup the controller for REST.
+ var rest = restful.model(
+   'meanmove',
+   app.models.meanmove
+ ).methods(['get', 'put', 'post', 'delete']);
 
-//register the restful API for the injected app into the controller
-  rest.register(app,route);
-//for every injection on the requestt we use custom features with the help of middleware
-   return function(req, res, next){
-     next();
-   };
+ // Register this endpoint with the application.
+ rest.register(app, route);
 
+ // Return middleware.
+ return function(req, res, next) {
+   next();
+ };
 };
